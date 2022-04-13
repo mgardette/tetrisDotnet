48fae0660f63183e42f1f3b8f79423f2d06afdff
ncoupey <74413792+ncoupey@users.noreply.github.com>
2022-04-13 09:09:02 +0200

    update

--- tetrisDotnet/tetrisDotnet/obj/Debug/netcoreapp3.1/view/MainWindow.g.i.cs
+++ tetrisDotnet/tetrisDotnet/obj/Debug/netcoreapp3.1/view/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace tetrisDotnet {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/tetrisDotnet;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/tetrisDotnet;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\view\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
